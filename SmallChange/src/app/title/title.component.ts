88b89a0e805288f72ed000521d7a053cd7b33f0a
Mitra, Shuvam <shuvammitra1999@gmail.com>
2022-09-05 14:17:14 +0530

    title update

--- SmallChange/src/app/title/title.component.ts
+++ SmallChange/src/app/title/title.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.css']
+  styleUrls: ['./title.component.css'],
 })
 export class TitleComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  @Input() paraText: String =
+    'Did you know that SmallChange <sup>TM</sup> has no investment fees on portfolios balances under $5,000? After that, only a 0.25% fees applies monthly.';
 }
