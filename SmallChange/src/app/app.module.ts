88d676042d5427f731ff7f849c1d2025a6aa7393
Adithya <adithyaharish10@gmail.com>
2022-09-05 11:51:55 +0530

    create component

--- SmallChange/src/app/app.module.ts
+++ SmallChange/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TitleComponent } from './title/title.component';
+import { FormComponent } from './form/form.component';
+import { PageComponent } from './page/page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    TitleComponent,
+    FormComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule
