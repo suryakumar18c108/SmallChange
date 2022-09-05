7e83e15ff02b5fad65c842a82de836235b642581
Adithya <adithyaharish10@gmail.com>
2022-09-05 12:21:39 +0530

    form update

--- SmallChange/src/app/app.module.ts
+++ SmallChange/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { TitleComponent } from './title/title.component';
@@ -12,7 +11,6 @@ import { PageComponent } from './page/page.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     HeaderComponent,
     FooterComponent,
     TitleComponent,
--- SmallChange/src/app/form/form.component.ts
+++ SmallChange/src/app/form/form.component.ts
@@ -8,8 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class FormComponent implements OnInit {
 
   constructor() { }
-
+  userBox: string = "Username";
+  
   ngOnInit(): void {
+
+
+    var user=document.getElementById("user")
+    var err_msg=document.getElementsByClassName("user-error")[0];
+
+    user?.addEventListener('keyup',(event)=>{
+      console.log(event);
+      /*
+      if(!(String(event.target.value).match(/^[a-zA-Z0-9-_\-]{3,18}$/))){
+        user?.classList.add('error-input')
+        err_msg?.style.display="block";
+        console.log(" In valid input")
+      }
+      else{
+        user?.classList.remove('error-input');
+        err_msg?.style.display="none";
+      }
+      */
+    })
+
+
   }
 
 }
